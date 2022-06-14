d99fd852d4fece9dad886adaf0e6318df992c133
kartikey <kartikeysood@rediffmail.com>
2022-06-14 14:16:11 +0530

    hello

--- server.js
+++ server.js
@@ -23,20 +23,6 @@ require("dotenv").config({ path: "./config.env" });
 
 // Routes
 
-// Sessions
-// const sess = {
-// 	secret: "No One KNOWS",
-// 	cookie: {},
-// 	secret: "keyboard cat",
-// 	resave: false,
-// 	saveUninitialized: true,
-// 	maxAge: 36000000, // 10hrs
-// };
-// if (app.get("env") === "production") {
-// 	app.set("trust proxy", 1); // trust first proxy
-// 	sess.cookie.secure = true; // serve secure cookies
-// }
-
 // Environment Variable
 const port = process.env.PORT;
 
@@ -49,22 +35,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan("dev"));
-// app.use(cookieParser());
-// app.use(session(sess));
 
 // Calling Routes
 
 //Restrict Invalid Routes
 app.get("*", (req, res) => {
-	console.log("Invalid Page Request");
-	res
-		.send("<h1><i><strong> ( 404 ) Page Not Found , Invalid page request")
-		.status(400);
+  console.log("Invalid Page Request");
+  res
+    .send("<h1><i><strong> ( 404 ) Page Not Found , Invalid page request")
+    .status(400);
 });
 
 /**========================================================================
  *                           Listening Port at 3000
  *========================================================================**/
 app.listen(port, () => {
-	console.log(`server is starting on port ${port}`);
+  console.log(`server is starting on port ${port}`);
 });
