f7aad25dfd670bb55599ea68780c998c801f8f1a
Jitesh_Suyal <jiteshasuyal@gmail.com>
2022-06-27 19:47:03 +0530

    fourth commit

--- src/route/user.route.js
+++ src/route/user.route.js
@@ -1,7 +1,21 @@
 const userRoute = require("express").Router();
 
-const { getUserById } = require("../controller/user.controller");
+const {
+  getUserById,
+  editProfile,
+  addAddress,
+  getUserAddress,
+  deleteAddress,
+} = require("../controller/user.controller");
 
 userRoute.get("/getUserById", getUserById);
+
+userRoute.patch("/editProfile", editProfile);
+
+userRoute.post("/addAddress", addAddress);
+
+userRoute.get("/getUserAddress", getUserAddress);
+
+userRoute.delete("/deleteAddress", deleteAddress);
 
 module.exports = userRoute;
