523514fca05392d4d918a31bc5047cb78f13c6d2
Jitesh_Suyal <jiteshasuyal@gmail.com>
2022-07-01 13:54:22 +0530

    1july1stCommit

--- src/model/user.Model.js
+++ src/model/user.Model.js
@@ -1,36 +1,49 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  userName: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true,
+const userSchema = new mongoose.Schema(
+  {
+    UID: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+    state: {
+      type: Boolean,
+      default: false,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "client", "delivery"],
+    },
+    notificationToken: {
+      type: String,
+      default: "",
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    lowercase: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  personId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Person",
-  },
-  role: {
-    type: String,
-    enum: ["admin", "client", "delivery"],
-  },
-  notificationToken: {
-    type: String,
-    default: "",
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
