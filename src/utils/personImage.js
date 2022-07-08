eb0f5a6213cc99df7a7d14efe304cc02a9ed81fc
kartikey <kartikeysood@rediffmail.com>
2022-07-08 14:24:05 +0530

    mergedd

--- src/utils/personImage.js
+++ src/utils/personImage.js
@@ -3,32 +3,32 @@ const path = require("path");
 const hash = require("random-hash");
 
 const storage = multer.diskStorage({
-	destination: function (req, file, cb) {
-	  cb(null, path.join(__dirname, "../../public/product"));
-	},
-	filename: function (req, file, cb) {
-	  let temp = file.originalname.split(".");
-	  const filename =
-		temp[0] + "-" + hash.generateHash({ length: 5 }) + "." + temp[1];
-	  cb(null, filename);
-	},
-  });
-  
-  const fileFilter = function (req, file, callback) {
-	var ext = path.extname(file.originalname);
-	if (ext !== ".png" && ext !== ".jpg" && ext !== ".jpeg") {
-	  return callback(new Error("Only images are allowed"));
-	}
-	callback(null, true);
-  };
-  
-  const upload = multer({
-	storage: storage,
-	limits: {
-	  fileSize: 1024 * 1024 * 5,
-	},
-	fileFilter: fileFilter,
-  });
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "../../public/product"));
+  },
+  filename: function (req, file, cb) {
+    let temp = file.originalname.split(".");
+    const filename =
+      temp[0] + "-" + hash.generateHash({ length: 5 }) + "." + temp[1];
+    cb(null, filename);
+  },
+});
+
+const fileFilter = function (req, file, callback) {
+  var ext = path.extname(file.originalname);
+  if (ext !== ".png" && ext !== ".jpg" && ext !== ".jpeg") {
+    return callback(new Error("Only images are allowed"));
+  }
+  callback(null, true);
+};
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5,
+  },
+  fileFilter: fileFilter,
+});
 
 // const storage = multer.diskStorage({
 // 	destination: async (req, file, cb) => {
