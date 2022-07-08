d28bef4cdd8145a8a430210f0267ed8842390931
kartikey <kartikeysood@rediffmail.com>
2022-07-08 14:49:22 +0530

    removed userImages

--- src/utils/personImage.js
+++ src/utils/personImage.js
@@ -30,20 +30,4 @@ const upload = multer({
   fileFilter: fileFilter,
 });
 
-// const storage = multer.diskStorage({
-// 	destination: async (req, file, cb) => {
-// 		cb(null, path.join(__dirname, "../public/image"), async (err, success) => {
-// 			if (err) throw err;
-// 		});
-// 	},
-// 	filename: async (req, file, cb) => {
-// 		const name = Date.now() + "-" + file.originalname;
-// 		cb(null, name, async (error, success) => {
-// 			if (error) throw error;
-// 		});
-// 	},
-// });
-
-// const upload = multer({ storage: storage });
-
 module.exports = upload;
