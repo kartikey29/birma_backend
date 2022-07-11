561e78ab167318e0698df569219c9e9f92be0334
Imxavi31081999 <imravishankar00@gmail.com>
2022-07-11 17:54:59 +0530

    change in add order

--- src/controller/order.controller.js
+++ src/controller/order.controller.js
@@ -19,17 +19,21 @@ const getOptions = (page) => {
 const addOrder = async (req, res, next) => {
   try {
     //console.log(req.body);
-    const { clientId } = req.body;
-    const checkUserId = await User.findById(clientId);
-    if (checkUserId) {
+    const { role_Id } = req.user;
+    const checkRole_Id = await User.findOne({ role_Id: role_Id });//checking role_id
+    if (checkRole_Id.role_Id === '1') {
+      const clientId=checkRole_Id._id;
+      req.body.clientID = clientId; //inserting user _id into clientId
+     //console.log(req.body);
       const insertData = await Order.create(req.body);
+      //console.log(insertData);
       return res.status(200).json({
         message: "Order created successfully",
         data: insertData,
-      });
+      })
     }
     else {
-      return res.status(404).json({ message: "clientId not found" });
+      return res.status(404).json({ message: "role_id doesn't match with clientID" });
     }
   } catch (error) {
     return res.status(500).json({
